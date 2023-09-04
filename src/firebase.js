0823428cbab803492b4fcc586c910f41e94d9cab
Desalegn Yismaw <DesalegnYi@ethiopianairlines.com>
2023-09-04 16:53:09 +0300

    initial-commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDbnwmNipog7M6s8KkGrXHTqHbNvHh6oeI",
+  authDomain: "clone-52eb5.firebaseapp.com",
+  projectId: "clone-52eb5",
+  storageBucket: "clone-52eb5.appspot.com",
+  messagingSenderId: "200034715839",
+  appId: "1:200034715839:web:b86bf97e60a55df1ef5e0a",
+  measurementId: "G-Z0J5D19Q7N"
+};
